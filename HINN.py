b1177e413340d3723f53a94969a362b5d709ecc5
Ayyuce Demirbas <a.ayyuced@gmail.com>
2025-09-25 21:53:26 +0300

    Update HINN.py

--- HINN.py
+++ HINN.py
@@ -16,7 +16,6 @@ class MaskedLinear(nn.Module):
             self.register_parameter('bias', None)
 
         self.register_buffer('mask', mask.to(torch.float32))
-
         nn.init.xavier_uniform_(self.weight)
 
     def forward(self, x):
@@ -27,11 +26,10 @@ class MaskedLinear(nn.Module):
         return out
 
 class FeatureScaler(nn.Module):
-    def __init__(self, features, eps=1e-6):
+    def __init__(self, features):
         super().__init__()
         self.features = features
         self.ws = nn.Parameter(torch.ones(features))
-        self.eps = eps
 
     def forward(self, x):
         return x * self.ws
@@ -45,39 +43,36 @@ class HINN(nn.Module):
                  mask_snp_cpg,
                  mask_cpg_gene,
                  mask_gene_go,
-                 demog_dim=6,  # APOE4, pTau, age, gender, education, race
+                 demog_dim=6,
+                 fc_nodes=20,
                  hidden_dense_units=128,
                  dropout_prob=0.7):
         super().__init__()
 
-        self.mask_snp_cpg = mask_snp_cpg
-        self.mask_cpg_gene = mask_cpg_gene
-        self.mask_gene_go = mask_gene_go
-
         self.snp_to_cpg = MaskedLinear(snp_dim, cpg_dim, mask_snp_cpg, bias=True)
-
         self.g_cpg = FeatureScaler(cpg_dim)
-        self.cpg_dense20 = nn.Linear(cpg_dim, 20)
+        self.B2 = nn.Parameter(torch.zeros(cpg_dim))
 
         self.cpg_to_gene = MaskedLinear(cpg_dim, gene_dim, mask_cpg_gene, bias=True)
-
         self.g_gene = FeatureScaler(gene_dim)
-        self.gene_dense20 = nn.Linear(gene_dim + 20, 20)
+        self.B3 = nn.Parameter(torch.zeros(gene_dim))
 
         self.gene_to_go = MaskedLinear(gene_dim, go_dim, mask_gene_go, bias=True)
 
-        self.go_dense20 = nn.Linear(go_dim + 20, 20)
+        self.snp_fc20 = nn.Linear(snp_dim, fc_nodes)
+        self.cpg_fc20 = nn.Linear(cpg_dim + fc_nodes, fc_nodes)
+        self.gene_fc20 = nn.Linear(gene_dim + fc_nodes, fc_nodes)
 
-        self.bn_layers = nn.ModuleList([nn.BatchNorm1d(hidden_dense_units, momentum=0.9, eps=0.005)
-                                        for _ in range(4)])
-        self.dense_layers = nn.ModuleList([nn.Linear(hidden_dense_units if i>0 else (go_dim + 20), hidden_dense_units)
-                                           for i in range(4)])
-        self.dropout = nn.Dropout(dropout_prob)
+        dense_input_dim = go_dim + fc_nodes
+        self.dense_block1 = self._build_dense_block(dense_input_dim, hidden_dense_units, dropout_prob)
+        self.dense_block2 = self._build_dense_block(hidden_dense_units, hidden_dense_units, dropout_prob)
+        self.dense_block3 = self._build_dense_block(hidden_dense_units, hidden_dense_units, dropout_prob)
+        self.dense_block4 = self._build_dense_block(hidden_dense_units, hidden_dense_units, dropout_prob)
+        
         self.post_dense20 = nn.Linear(hidden_dense_units, 20)
 
-        self.demog_bn = nn.BatchNorm1d(20 + demog_dim, momentum=0.9, eps=0.005)
-        self.demog_dense = nn.Linear(20 + demog_dim, hidden_dense_units)
-        self.final_dropout = nn.Dropout(dropout_prob)
+        demog_input_dim = 20 + demog_dim
+        self.demog_block = self._build_dense_block(demog_input_dim, hidden_dense_units, dropout_prob)
         self.output_layer = nn.Linear(hidden_dense_units, 1)
 
         for m in self.modules():
@@ -86,44 +81,47 @@ class HINN(nn.Module):
                 if m.bias is not None:
                     nn.init.zeros_(m.bias)
 
-    def forward(self, x_snp, x_cpg, x_gene, demog, eps=1e-6):
+    def _build_dense_block(self, in_features, out_features, dropout_prob):
+        return nn.Sequential(
+            nn.BatchNorm1d(in_features, momentum=0.9, eps=0.005),
+            nn.Linear(in_features, out_features),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob)
+        )
+
+    def forward(self, x_snp, x_cpg, x_gene, demog, eps=1e-8):
         Y1 = F.relu(self.snp_to_cpg(x_snp))
 
-        #DNA methylation (CpG) layer
         gX2 = self.g_cpg(x_cpg)
-        Y2_pre = gX2 * Y1
-        Y2 = F.relu(Y2_pre)
-        cpg_fc20 = F.relu(self.cpg_dense20(gX2))
+        Y2 = F.relu((gX2 * Y1) + self.B2)
 
-        fY2 = F.relu(self.cpg_to_gene(Y2))
-
-        #Gene layer
         gX3 = self.g_gene(x_gene)
+
+        fY2 = self.cpg_to_gene(Y2)
         denom = fY2 + eps
-        Y3_pre = gX3 / denom
-        Y3 = F.relu(Y3_pre)
-        gene_fc_in = torch.cat([Y3, cpg_fc20], dim=1)
-        gene_fc20 = F.relu(self.gene_dense20(gene_fc_in))
+        Y3 = F.relu((gX3 / denom) + self.B3)
 
-        #GO layer
         Y4 = F.relu(self.gene_to_go(Y3))
-        go_fc_in = torch.cat([Y4, gene_fc20], dim=1)
-        go_fc20 = F.relu(self.go_dense20(go_fc_in))
 
-        dense_input = torch.cat([Y4, go_fc20], dim=1)
-        h = dense_input
-        for bn, dense in zip(self.bn_layers, self.dense_layers):
-            h = dense(h)
-            h = bn(h)
-            h = F.relu(h)
-            h = self.dropout(h)
+        snp_fc_out = F.relu(self.snp_fc20(x_snp))
+        
+        cpg_fc_in = torch.cat([Y2, snp_fc_out], dim=1)
+        cpg_fc_out = F.relu(self.cpg_fc20(cpg_fc_in))
+        
+        gene_fc_in = torch.cat([Y3, cpg_fc_out], dim=1)
+        gene_fc_out = F.relu(self.gene_fc20(gene_fc_in))
+
+        dense_input = torch.cat([Y4, gene_fc_out], dim=1)
+        
+        h = self.dense_block1(dense_input)
+        h = self.dense_block2(h)
+        h = self.dense_block3(h)
+        h = self.dense_block4(h)
 
         h20 = F.relu(self.post_dense20(h))
 
         demog_in = torch.cat([h20, demog], dim=1)
-        demog_in = self.demog_bn(demog_in)
-        demog_h = F.relu(self.demog_dense(demog_in))
-        demog_h = self.final_dropout(demog_h)
-
+        demog_h = self.demog_block(demog_in)
+        
         out = self.output_layer(demog_h)
         return out.squeeze(-1)
